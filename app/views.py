9a651746b958c26d1d26e22194bd55493c6b7a6d
Emil Gabrielli <emilgabrielli@gmail.com>
2020-10-30 00:35:30 +0100

    Added navigation bar

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import Flask, render_template
 
+nav_items = {
+    "HOME":"#home",
+    "BUY TICKET":"#purchase",
+    "SHOW TICKETS":"#show",
+    "YOUR PROFILE":"#profile",
+    "ABOUT US":"#about",
+    "LOG IN":"#login"
+}
+
 @app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template("homepage.html", nav_items=nav_items)
