c033acc7f2ed0830d5f274d2cf6abebc0145b3a3
Emil Gabrielli <emilgabrielli@gmail.com>
2020-11-15 03:53:36 +0100

    Edited form and some misc fixes

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField,DateTimeField,IntegerField,validators
+from wtforms import StringField,SubmitField,PasswordField,DateTimeField,IntegerField,SelectField
+from wtforms import validators
 from datetime import datetime
+
+stations = ["Oslo","Fredrikstad","Halden","Bergen","Tromsø","Stavanger","Molde","Kristiansand","Lillehammer"]
+stations.sort()
+destinations = [(x,x) for x in stations]
 
 class Login(FlaskForm):
 
@@ -9,8 +14,8 @@ class Login(FlaskForm):
     submit = SubmitField('Log in')
 
 class PurchaseTicket(FlaskForm):
-    start_station = StringField(u"Start station:", validators=[validators.DataRequired()])
-    end_station = StringField(u"End station:", validators=[validators.DataRequired()])
+    start_station = SelectField(u"Start station:", choices=destinations, validators=[validators.DataRequired()])
+    end_station = SelectField(u"End station:", choices=destinations, validators=[validators.DataRequired()])
     travel_date = DateTimeField(u"Travel date:", format="%d/%m/%Y", default=datetime.today, validators=[validators.DataRequired()])
     number_of_passengers = IntegerField(u"Passengers:", validators=[validators.DataRequired()])
     submit = SubmitField("Go to payment")
