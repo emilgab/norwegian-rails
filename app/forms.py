bba2aebd3e2fa239c40beb1656406c99d9f6a6fa
Emil Gabrielli <emilgabrielli@gmail.com>
2020-11-17 04:07:55 +0100

    Added more sections for the purchase ticket page - When selecting the travel date, you will now get the browsers datepicket (not supported in safari and IE) - Added seat reservations 	- Used radio buttons to select one option 	- Added icons representing each option - Added additional services 	- Used checkboxes for the option to multiple select 	- Added icons representing each option - Edited some spacing between the sections - Added customisation for the hr-tag

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField,DateTimeField,IntegerField,SelectField
+from wtforms import StringField,SubmitField,PasswordField,IntegerField,SelectField,RadioField,SelectMultipleField,widgets
+from wtforms.fields.html5 import DateField
 from wtforms import validators
 from datetime import datetime
 
@@ -13,9 +14,25 @@ class Login(FlaskForm):
     password = PasswordField(u"Password:")
     submit = SubmitField('Log in')
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
 class PurchaseTicket(FlaskForm):
     start_station = SelectField(u"Start station:", choices=destinations, validators=[validators.DataRequired()])
     end_station = SelectField(u"End station:", choices=destinations, validators=[validators.DataRequired()])
-    travel_date = DateTimeField(u"Travel date:", format="%d/%m/%Y", default=datetime.today, validators=[validators.DataRequired()])
+    travel_date = DateField(u"Travel date:", format="%d/%m/%Y", default=datetime.today, validators=[validators.DataRequired()])
     number_of_passengers = IntegerField(u"Passengers:", validators=[validators.DataRequired()])
+    seat_reservation = RadioField('Seat reservation',choices=[
+                        ('none','No reservations'),
+                        ('hc','Wheelchair accessible'),
+                        ('comfort','Comfort cart seat'),
+                        ('sleep','Sleeping quarters')
+                        ], default='none', validators=[validators.DataRequired()])
+    add_ons = MultiCheckboxField(choices = [
+                        ('chicken','Food serving: Chicken'),
+                        ('fish','Food serving: Fish'),
+                        ('dog','Bring your dog'),
+                        ('skis','Bring your ski equipment')
+    ])
     submit = SubmitField("Go to payment")
