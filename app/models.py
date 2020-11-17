89a606b8afaad9e349d5127d17ec4f3cd50dad66
Emil Gabrielli <emilgabrielli@gmail.com>
2020-11-17 21:40:50 +0100

    Added login functionalities and database setup - Created the login manager that will manage the login and logout of users - Created the database called data.sqlite, not yet integrated in the app

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
+class Users(db.Model,UserMixin):
+    __tablename__ = u"users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String, unique=False)
+    fullname = db.Column(db.String, unique=False)
+
+    def __init__(self,username,password,fullname):
+        self.username=username
+        self.password=password
+        self.fullname=fullname
+
+class Ticket(db.Model):
+    __tablename__ = u"tickets"
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String, unique=False)
+    username = db.Column(db.String, unique=False)
+    start_station = db.Column(db.String, unique=False)
+    end_station = db.Column(db.String, unique=False)
+    date = db.Column(db.String, unique=False)
+    passengers = db.Column(db.String, unique=False)
+    seat_reservation = db.Column(db.String, unique=False)
+    add_ons = db.Column(db.String, unique=False)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import Flask, render_template, session, redirect, url_for
 from app.forms import Login, PurchaseTicket, QuickRegister, EntryAuth
+from flask_login import login_user, login_required, logout_user
 
 nav_items = {
     "HOME":"homepage",
     "BUY TICKET":"purchase",
-    #"SHOW TICKETS":"show_tickets",
-    #"YOUR PROFILE":"profile",
+    "SHOW TICKETS":"show_tickets",
+    "YOUR PROFILE":"profile",
     "ABOUT US":"about",
     "LOG IN":"login"
 }
@@ -20,27 +21,67 @@ def homepage():
 
 @app.route('/purchase')
 def purchase():
-    form = PurchaseTicket()
-    return render_template("purchase_tickets.html", nav_items=nav_items, form=form)
+    if session.get('access_granted') == True:
+        form = PurchaseTicket()
+        return render_template("purchase_tickets.html", nav_items=nav_items, form=form)
+    else:
+        return redirect(url_for('access_site'))
 
 @app.route('/show_tickets')
+@login_required
 def show_tickets():
-    return render_template("show_tickets.html", nav_items=nav_items)
+    if session.get('access_granted') == True:
+        return render_template("show_tickets.html", nav_items=nav_items)
+    else:
+        return redirect(url_for('access_site'))
 
 @app.route('/profile')
+@login_required
 def profile():
-    return render_template("profile.html", nav_items=nav_items)
+    if session.get('access_granted') == True:
+        return render_template("profile.html", nav_items=nav_items)
+    else:
+        return redirect(url_for('access_site'))
 
 @app.route('/about')
 def about():
-    return render_template("about.html", nav_items=nav_items)
+    if session.get('access_granted') == True:
+        return render_template("about.html", nav_items=nav_items)
+    else:
+        return redirect(url_for('access_site'))
 
 @app.route('/login')
 def login():
-    # forms
-    form = Login()
-    register = QuickRegister()
-    return render_template("login.html", nav_items=nav_items, form=form, register=register)
+    if session.get('access_granted') == True:
+        # forms
+        form = Login()
+        register = QuickRegister()
+        if form.validate_on_submit():
+            try:
+                user.Users.query.filter_by(username=form.username.data).first()
+            except:
+                pass
+            try:
+                if user.password == form.password.data:
+                    login_user(user)
+                    next = request.args.get('next')
+                    if next == None or not next[0] == '/':
+                        next = url_for('home')
+                    return redirect(next)
+            except:
+                pass
+        return render_template("login.html", nav_items=nav_items, form=form, register=register)
+    else:
+        return redirect(url_for('access_site'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    if session.get('access_granted') == True:
+        logout_user()
+        return redirect(url_for('homepage'))
+    else:
+        return redirect(url_for('access_site'))
 
 @app.route('/lock', methods=['GET','POST'])
 def access_site():
